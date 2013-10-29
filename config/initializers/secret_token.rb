1c4c8f1cb585004f575387e4117adb7820069309
Marcus Petzoldt <marcus.petzoldt@gmail.com>
2013-10-29 20:09:00 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Skimfy::Application.config.secret_key_base = '814a92af2cc17b5f215f21d39f781c697802a1bd8a99e6b10cfb712ce25f868a5deb85ae06633fd590c72c545b328af15364c22b6ee6acf6fc1e0eb7424bb60e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Skimfy::Application.config.session_store :cookie_store, key: '_skimfy_session'
